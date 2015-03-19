90542a207d428c86e0a3d81b2f94b6f37a647525
Shane Steszyn <shane@steszyn.ca>
2015-03-19 14:24:06 -0400

    Fixed some errors

--- Server/server.js
+++ Server/server.js
@@ -10,18 +10,13 @@ var app = express();
 // Connect to mongo db
 mongo.connect('mongodb://localhost/oop');
 
-var Account = mongoose.model('accounts', { username: String, password: String }); // Account id is _id
-Account.plugin(autoIncrement.plugin, 'accounts');
-var Picture = mongoose.model('pictures', { account_id: Number, encoded_string: String }); //Picture id is _id
+var Account = mongo.model('accounts', { username: String, password: String }); // Account id is _id
+var Picture = mongo.model('pictures', { account_id: Number, encoded_string: String }); //Picture id is _id
 
 // Log all reqs to console (terminal)
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
-});
-
-app.use('/api', function(req, res, next){
-    res.send("NICE JOB");
 });
 
 app.use('/api/accounts/create', function(req, res, next){
@@ -35,6 +30,10 @@ app.use('/api/accounts/create', function(req, res, next){
   });
 
   res.send(account);
+});
+
+app.use('/api', function(req, res, next){
+    res.send("NICE JOB");
 });
 
 console.log('Starting a server on port 8080');
