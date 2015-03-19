5eb7e19dcc45fdedbdf73a967f3c6d2037427ee5
Shane Steszyn <shane@steszyn.ca>
2015-03-19 14:20:35 -0400

    Removed auto-increment, added sample create

--- Server/server.js
+++ Server/server.js
@@ -6,22 +6,35 @@ var http = require('http');
 var mongo = require('mongoose');
 
 var app = express();
+
+// Connect to mongo db
 mongo.connect('mongodb://localhost/oop');
 
-var Accounts = mongoose.model('accounts', { account_id: String });
+var Account = mongoose.model('accounts', { username: String, password: String }); // Account id is _id
+Account.plugin(autoIncrement.plugin, 'accounts');
+var Picture = mongoose.model('pictures', { account_id: Number, encoded_string: String }); //Picture id is _id
 
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) // ...
-  console.log('meow');
-});
+// Log all reqs to console (terminal)
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
 });
 
 app.use('/api', function(req, res, next){
-	res.send("NICE JOB");
+    res.send("NICE JOB");
+});
+
+app.use('/api/accounts/create', function(req, res, next){
+  // Sample create
+  var account = new Account({ username: 'Shane', password: 'pass' });
+
+  // Save this account to mongo
+  account.save(function (err) {
+    if (err)
+      console.log(err);
+  });
+
+  res.send(account);
 });
 
 console.log('Starting a server on port 8080');
