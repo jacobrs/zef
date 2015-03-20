25c4ddb3e27b6b08b5c003f4bcd32c55a0e453a8
jacobrs <jacob_gagne@hotmail.com>
2015-03-20 17:55:08 -0400

    added more material design features

--- Draw Android/app/src/main/java/com/steszyngagne/draw/Login.java
+++ Draw Android/app/src/main/java/com/steszyngagne/draw/Login.java
@@ -3,6 +3,7 @@ package com.steszyngagne.draw;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +27,18 @@ public class Login extends Activity {
             public void onClick(View v) {
                 // launch login
                 login();
+            }
+        });
+
+        root.findViewById(R.id.password).setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    login();
+                }
+                return false;
             }
         });
 
