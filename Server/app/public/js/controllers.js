8f62e6e16368f7daf2bdbc98062eb899dafc4867
Shane Steszyn <shane@steszyn.ca>
2015-04-04 12:28:52 -0400

    Angular https

--- Server/app/public/js/controllers.js
+++ Server/app/public/js/controllers.js
@@ -6,12 +6,25 @@ appControllers.controller('AppCtrl', ['$scope', '$timeout', '$mdSidenav', '$http
       $mdSidenav(menuId).toggle();
     };
 
-    $http.get('/api/pictures/public').success(function(result) {      
-      $scope.pictures = result;
-      console.log(result);
-    }).error(function(err) {
+    $scope.public = function(){ // Get all public pics
+      $http.get('/api/pictures/public').success(function(result) {      
+        $scope.pictures = result;
+        console.log(result);
+      }).error(function(err) {
 
-    });
+      });
+    };
+
+    $scope.private = function(){ // Get all private pics
+      $http.get('/api/pictures/').success(function(result) {      
+        $scope.pictures = result;
+        console.log(result);
+      }).error(function(err) {
+
+      });
+    };
+
+    $scope.public(); // Load all public by devailt    
      
   }
 ]);
